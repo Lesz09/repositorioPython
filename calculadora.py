2f3615c67c39584449a8b0789c7dbfc8d0180237
Lesz <Lesz_09@outlook.com>
2024-05-25 10:45:30 -0600

    Correción de nomenclatura

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
 # Escribe un programa que convierta una temperatura en grados Celsius a grados Fahrenheit. La fórmula es: Fahrenheit = Celsius * 9/5 + 32.
-n = float(input("Centigrados ->"+" "))
-formula = n * 9/5 + 32
-print("Grados fahrenheit -> 24.5"+str(formula))
+N = float(input("Centigrados ->"+" "))
+FORMULA = N * 9/5 + 32
+print("Grados fahrenheit -> 24.5"+str(FORMULA))
