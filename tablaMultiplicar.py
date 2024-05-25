580632a4ad2bb82d859306b7830ac5091851d0b2
Lesz <Lesz_09@outlook.com>
2024-05-25 11:27:16 -0600

    Codificación

---
+++ tablaMultiplicar.py
@@ -0,0 +1,6 @@
+contador = 1
+multiplicador = int(input("Ingrese un número entero:"+" "))
+while contador <= 10:
+    print(contador * multiplicador)
+    contador = contador + 1
+print("Fin programa")
